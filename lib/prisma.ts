57b881bd709757deb19c15e3125e087f95e668da
Jack Pitts <jackpitts@Jacks-MBP-3.lan>
2026-04-21 00:42:15 -0400

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,1 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-
-function createPrismaClient() {
-  const adapter = new PrismaBetterSqlite3({ url: "file:./dev.db" });
-  return new PrismaClient({ adapter } as any);
-}
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? createPrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma } from "./db";
